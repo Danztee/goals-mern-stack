8e7f158913d4314985217157b1f9bed9ade3b21b
Danztee <olowoniyidaniel@gmail.com>
2023-03-13 21:36:44 +0100

    update

--- backend/server.js
+++ backend/server.js
@@ -20,19 +20,19 @@ app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
 // serve frontend
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "dist", "index.html")
-    )
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("set to production");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(
+//       path.resolve(__dirname, "../", "frontend", "dist", "index.html")
+//     )
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("set to production");
+//   });
+// }
 
 app.get("/", (req, res) => {
   res.send("welcome to the API");
